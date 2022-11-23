ebbbcddd4d14a86e9e18e3a4862bda9703929809
Abdul Rehman <81208506+AR-py@users.noreply.github.com>
2022-11-23 19:24:45 +0530

    Add files via upload

--- agecalc.py
+++ agecalc.py
@@ -1,25 +1,27 @@
 from tkinter import *
-import sv_ttk
-from tkinter import ttk
+from dark_title_bar import *
 
 w = Tk()
 w.geometry('400x200')
-sv_ttk.set_theme('light')
 w.title('AgeCalc')
+w.configure(bg='#1c1c1c')
+w.resizable(False,False)
+w.iconbitmap('icon.ico')
+dark_title_bar(w)
 
-l = ttk.Label(w, text='Year of Birth')
+l = Label(w, text='Year of Birth', background='#1c1c1c', foreground='white')
 l.pack(pady=10)
 
-e = ttk.Entry(w)
+e = Entry(w, background='#242424', foreground='white')
 e.pack(pady=10)
 
 def submit():
     l1.config(text="Your Age: " + f'{2022-int(str(e.get()))}')
     
-l1 = ttk.Label(w, text='')
+l1 = Label(w, text='', background='#1c1c1c', foreground='white')
 l1.pack(pady=4)
 
-b = ttk.Button(w, text='Submit', command=submit)
+b = Button(w, text='Submit', command=submit, background='#242424', foreground='white')
 b.pack(pady=6)
 
 w.mainloop()
